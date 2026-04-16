2f0e2d54fcf214108db8847f748cdb057de11255
altaiezior <6075379+altaiezior@users.noreply.github.com>
2026-04-17 03:38:49 +0530

    Switched `AudienceProfileMapper` from abstract class to interface

--- issue-4037/src/main/java/org/example/AudienceProfileMapper.java
+++ issue-4037/src/main/java/org/example/AudienceProfileMapper.java
@@ -4,11 +4,11 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(config = CentralConfig.class)
-abstract class AudienceProfileMapper {
+interface AudienceProfileMapper {
     @Mapping(target = "deviceId", source = "requestBean.deviceId")
     @Mapping(target = "browserId", source = "requestBean.deviceInfo.browserId")
     @Mapping(target = "osId", source = "requestBean.deviceInfo.osId")
     @Mapping(target = "country", source = "requestBean.geoBean.country")
     @Mapping(target = "stateCode", source = "requestBean.geoBean.stateCode")
-    public abstract AudienceProfileRequest map(RequestBean requestBean, String slotId);
+    AudienceProfileRequest map(RequestBean requestBean, String slotId);
 }
